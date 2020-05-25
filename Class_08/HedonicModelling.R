7c5554d92ffff412c983d0385832667fe6dd5a57
Esteban Lopez <lopezochoa.estebanalejandro@gmail.com>
2020-05-25 02:31:31 -0400

    Uploading Class 09 contents
    ShinyApps and Flexdashboards

--- Class_08/HedonicModelling.R
+++ Class_08/HedonicModelling.R
@@ -13,7 +13,7 @@ packages<-c('McSpatial','sp','devtools','rgeos')
 sapply(packages,FUN = require,character.only=T)
 
 # Calculando distancias
-# Euclidianas
+# Euclidianas (en línea recta)
 coords@data$distEu_av_sat<-geoshape(longvar = coords@data$coords.2.,latvar = coords@data$coords.1.,pointfile = averdes)*0.621371
 coords@data$distEu_av_prc<-geoshape(longvar = coords@data$coords.2.,latvar = coords@data$coords.1.,pointfile = prc_averdes)*0.621371
 
